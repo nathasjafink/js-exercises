e39159560c1692c4a10b86ee1d0f4ec1f857ef15
nathasjafink <nathasja.nielsen@gmail.com>
2023-10-06 11:32:44 +0200

    Exercise 1-6

--- week-5/InClass/A-dom-manipulation/exercise.js
+++ week-5/InClass/A-dom-manipulation/exercise.js
@@ -15,6 +15,21 @@ Write JavaScript below that logs:
     --> should log a list of nodes with a length of 3
 
 */
+// 1
+const pElement = document.querySelectorAll("p");
+console.log(pElement);
+
+// 2
+const divElement = document.querySelector(".site-header");
+console.log(divElement);
+
+// 3
+const divElement2 = document.querySelector("#jumbotron-text");
+console.log(divElement2);
+
+// 4
+const pPrimaryContent = document.querySelector(".primary-content").querySelectorAll("p");
+console.log(pPrimaryContent);
 
 
 /*
@@ -23,7 +38,10 @@ Task 2
 
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
-
+const alertButton = document.querySelector("#alertBtn");
+alertButton.addEventListener("click",function(){
+    console.log(alert("Thanks for visiting Bikes for Refugees!"));
+});
 
 /*
 Task 3
@@ -31,7 +49,10 @@ Task 3
 
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
-
+const changeColourBottom = document.querySelector("#bgrChangeBtn");
+changeColourBottom.addEventListener("click",function (){
+    document.body.style.backgroundColor = "#8B0000";
+});
 
 /*
 Task 4
@@ -39,7 +60,10 @@ Task 4
 
 When a user clicks the 'Add some text' button, a new paragraph should be added below the buttons that says "Read more below."
 */
-
+const addSomeTextBottom = document.querySelector("#addTextBtn");
+addSomeTextBottom.addEventListener("click",function (){
+    document.querySelector(".buttons").insertAdjacentHTML('afterend','<h2>Read more below</h2>')
+});
 
 
 /*
@@ -48,3 +72,24 @@ Task 5
 
 When the 'Larger links!' button is clicked, the text of all links on the page should increase.
 */
+
+const largerLinksBottom = document.querySelector("#largerLinksBtn");
+largerLinksBottom.addEventListener("click",function (){
+    document.querySelectorAll("a").forEach(function (link){
+        link.style.fontSize = "larger";
+    })
+});
+
+/* Exercise 6
+When clicking the blue add button, take the text written in the input element, and add that text to the DOM.
+The text should be added using ul and li
+ */
+const addButtom = document.querySelector("#addArticleBtn");
+
+addButtom.addEventListener("click",function (){
+    const input = document.querySelector(".addArticle").value;
+    document.querySelector(".heading-underline").insertAdjacentHTML('beforebegin',"<ul class = 'ul' ></ul>");
+    const liElement = document.createElement("li");
+    liElement.innerText = input;
+    document.querySelector(".ul").append(liElement);
+})
